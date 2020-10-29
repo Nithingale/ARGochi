6f1240e648b21e91219382b5aad2aed7e39614a4
Vleb <vic.lbc@live.fr>
2020-10-29 16:16:26 +0100

    [+] Scene buttons backend [+] Left-handed mode

---
+++ Assets/Scripts/HandSetting/HandSettingToggler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandSettingToggler : MonoBehaviour
+{
+    private Toggle toggle;
+
+    void Start() {
+        toggle = GetComponent<Toggle>();
+        toggle.isOn = HandSettingData.leftHanded;
+        toggle.onValueChanged.AddListener((bool isOn) => {
+            HandSettingData.leftHanded = isOn;
+        });
+    }
+}
--- Assets/Scripts/StaticClass.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class StaticClass
-{
-    public static GameObject CrossSceneInformation { get; set; }
-}

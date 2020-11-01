b41bf43c3dc3d4341cdf4344a3f75a99a90658b5
roymarmotte <valentin.maeght@epitech.eu>
2020-11-01 18:09:15 +0100

    ajout d'une interaction tactile avec Chaos

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public float force = 10.0f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                rb.AddForce(transform.forward * force);
+            }
+        }
+    }
+}
